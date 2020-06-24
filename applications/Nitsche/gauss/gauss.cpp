0d256aa06472e4bb432eb32ffac051e216a010d5
eaulisa <eugenio.aulisa@ttu.edu>
2020-06-24 12:18:21 -0500

    added wegge gpoint

--- applications/Nitsche/gauss/gauss.cpp
+++ applications/Nitsche/gauss/gauss.cpp
@@ -38,15 +38,15 @@ int main(int argc, char** args) {
 //     std::cout << size << ", ";
 //   }
 //   std::cout << "\b\b};\n\n";
-// 
+//
 //   std::cout << "const double * quad_gauss::Gauss[" << maxNG << "] = {";
 //   for(unsigned ng = 1; ng <= maxNG; ng++) {
 //     std::cout << "Gauss" << ng - 1 << "[0], ";
 //   }
 //   std::cout << "\b\b};\n\n";
-// 
+//
 //   for(unsigned ng = 1; ng <= maxNG; ng++) {
-// 
+//
 //     unsigned size = pow(ng, dim);
 //     std::vector < double> weight(size);
 //     std::vector < double> x(size);
@@ -66,44 +66,44 @@ int main(int argc, char** args) {
 //       }
 //     }
 //     std::cout << "\b\b},\n{";
-// 
+//
 //     for(unsigned i = 0; i < ng; i++) {
 //       for(unsigned j = 0; j < ng; j++) {
 //         std::cout << x[i * ng + j] << ", ";
 //       }
 //     }
 //     std::cout << "\b\b},\n{";
-// 
+//
 //     for(unsigned i = 0; i < ng; i++) {
 //       for(unsigned j = 0; j < ng; j++) {
 //         std::cout << y[i * ng + j] << ", ";
 //       }
 //     }
 //     std::cout << "\b\b}};" << std::endl << std::endl;
-// 
+//
 //   }
-// 
-// 
-// 
+//
+//
+//
 //   //HEX
 //   dim = 3;
 //   maxNG = 1;
-// 
+//
 //   std::cout << "const unsigned hex_gauss::GaussPoints[" << maxNG << "] = {";
 //   for(unsigned ng = 1; ng <= maxNG; ng++) {
 //     unsigned size = pow(ng, dim);
 //     std::cout << size << ", ";
 //   }
 //   std::cout << "\b\b};\n\n";
-// 
+//
 //   std::cout << "const double * hex_gauss::Gauss[" << maxNG << "] = {";
 //   for(unsigned ng = 1; ng <= maxNG; ng++) {
 //     std::cout << "Gauss" << ng - 1 << "[0], ";
 //   }
 //   std::cout << "\b\b};\n\n";
-// 
+//
 //   for(unsigned ng = 1; ng <= maxNG; ng++) {
-// 
+//
 //     unsigned size = pow(ng, dim);
 //     std::vector < double> weight(size);
 //     std::vector < double> x(size);
@@ -121,6 +121,189 @@ int main(int argc, char** args) {
 //     }
 //     std::cout.precision(14);
 //     std::cout << "const double hex_gauss::Gauss" << ng - 1 << "[" << dim + 1 << "][" <<  size << "] = {{";
+//     for(unsigned i = 0; i < ng; i++) {
+//       for(unsigned j = 0; j < ng; j++) {
+//         for(unsigned k = 0; k < ng; k++) {
+//           std::cout << weight[i * ng * ng + j * ng + k]  << ", ";
+//         }
+//       }
+//     }
+//     std::cout << "\b\b},\n{";
+//
+//     for(unsigned i = 0; i < ng; i++) {
+//       for(unsigned j = 0; j < ng; j++) {
+//         for(unsigned k = 0; k < ng; k++) {
+//           std::cout << x[i * ng * ng + j * ng + k] << ", ";
+//         }
+//       }
+//     }
+//     std::cout << "\b\b},\n{";
+//
+//     for(unsigned i = 0; i < ng; i++) {
+//       for(unsigned j = 0; j < ng; j++) {
+//         for(unsigned k = 0; k < ng; k++) {
+//           std::cout << y[i * ng * ng + j * ng + k] << ", ";
+//         }
+//       }
+//     }
+//     std::cout << "\b\b},\n{";
+//
+//     for(unsigned i = 0; i < ng; i++) {
+//       for(unsigned j = 0; j < ng; j++) {
+//         for(unsigned k = 0; k < ng; k++) {
+//           std::cout << z[i * ng * ng + j * ng + k] << ", ";
+//         }
+//       }
+//     }
+//     std::cout << "\b\b}};" << std::endl << std::endl;
+//
+//   }
+//
+//
+// //triangle
+//
+//     std::vector < std::vector < double > > xv = {{0., 1., 0., 0.}, {0., 0., 1., 1.}};
+//     std::vector <double> phi;  // local test function
+//     std::vector <double> phi_x; // local test function first order partial derivatives
+//     double jac;
+//
+//     const elem_type *fem = new const elem_type_2D("quad", "linear", "seventh");
+//
+//     dim = 2;
+//     maxNG = 3;
+//
+//     std::cout << "const unsigned tri_gauss::GaussPoints[" << maxNG << "] = {";
+//     for(unsigned ng = 1; ng <= maxNG; ng++) {
+//       unsigned size = pow(ng, dim);
+//       std::cout << size << ", ";
+//     }
+//     std::cout << "\b\b};\n\n";
+//
+//     std::cout << "const double * tri_gauss::Gauss[" << maxNG << "] = {";
+//     for(unsigned ng = 1; ng <= maxNG; ng++) {
+//       std::cout << "Gauss" << ng - 1 << "[0], ";
+//     }
+//     std::cout << "\b\b};\n\n";
+//
+//     for(unsigned ng = 1; ng <= maxNG; ng++) {
+//
+//       unsigned size = pow(ng, dim);
+//       std::vector < double> weight(size);
+//       std::vector < double> x(size);
+//       std::vector < double> y(size);
+//
+//       std::vector < double > xi(dim);
+//       for(unsigned i = 0; i < ng; i++) {
+//         for(unsigned j = 0; j < ng; j++) {
+//           weight[i * ng + j] = Gauss[ng - 1][i] * Gauss[ng - 1][j];
+//           xi[0] = Gauss[ng - 1][ng + i];
+//           xi[1] = Gauss[ng - 1][ng + j];
+//
+//           fem->Jacobian(xv, xi, jac, phi, phi_x);
+//           weight[i * ng + j] *= jac;
+//
+//           x[i * ng + j] = 0.;
+//           y[i * ng + j] = 0.;
+//           for(unsigned ii = 0; ii < 4; ii++) {
+//             x[i * ng + j] += phi[ii] * xv[0][ii];
+//             y[i * ng + j] += phi[ii] * xv[1][ii];
+//           }
+//
+//
+//         }
+//       }
+//       std::cout.precision(14);
+//       std::cout << "const double tri_gauss::Gauss" << ng - 1 << "[" << dim + 1 << "][" <<  size << "] = {{";
+//       for(unsigned i = 0; i < ng; i++) {
+//         for(unsigned j = 0; j < ng; j++) {
+//           std::cout << weight[i * ng + j] << ", ";
+//         }
+//       }
+//       std::cout << "\b\b},\n{";
+//
+//       for(unsigned i = 0; i < ng; i++) {
+//         for(unsigned j = 0; j < ng; j++) {
+//           std::cout << x[i * ng + j] << ", ";
+//         }
+//       }
+//       std::cout << "\b\b},\n{";
+//
+//       for(unsigned i = 0; i < ng; i++) {
+//         for(unsigned j = 0; j < ng; j++) {
+//           std::cout << y[i * ng + j] << ", ";
+//         }
+//       }
+//       std::cout << "\b\b}};" << std::endl << std::endl;
+//
+//     }
+//
+//     delete fem;
+//
+//
+
+// //tetrahedra
+// 
+//   std::vector < std::vector < double > > xv = {{0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1}};
+//   std::vector <double> phi;  // local test function
+//   std::vector <double> phi_x; // local test function first order partial derivatives
+//   double jac;
+// 
+//   const elem_type *fem = new const elem_type_3D("hex", "linear", "seventh");
+// 
+//   dim = 3;
+//   maxNG = 3;
+// 
+//   std::cout << "const unsigned tet_gauss::GaussPoints[" << maxNG << "] = {";
+//   for(unsigned ng = 1; ng <= maxNG; ng++) {
+//     unsigned size = pow(ng, dim);
+//     std::cout << size << ", ";
+//   }
+//   std::cout << "\b\b};\n\n";
+// 
+//   std::cout << "const double * tet_gauss::Gauss[" << maxNG << "] = {";
+//   for(unsigned ng = 1; ng <= maxNG; ng++) {
+//     std::cout << "Gauss" << ng - 1 << "[0], ";
+//   }
+//   std::cout << "\b\b};\n\n";
+// 
+//   for(unsigned ng = 1; ng <= maxNG; ng++) {
+// 
+//     unsigned size = pow(ng, dim);
+//     std::vector < double> weight(size);
+//     std::vector < double> x(size);
+//     std::vector < double> y(size);
+//     std::vector < double> z(size);
+//     std::vector < double > xi(dim);
+// 
+//     for(unsigned i = 0; i < ng; i++) {
+//       for(unsigned j = 0; j < ng; j++) {
+//         for(unsigned k = 0; k < ng; k++) {
+//           weight[i * ng * ng + j * ng + k] = Gauss[ng - 1][i] * Gauss[ng - 1][j] * Gauss[ng - 1][k];
+//           xi[0] = Gauss[ng - 1][ng + i];
+//           xi[1] = Gauss[ng - 1][ng + j];
+//           xi[2] = Gauss[ng - 1][ng + k];
+// 
+// 
+//           fem->Jacobian(xv, xi, jac, phi, phi_x);
+// 
+//           weight[i * ng * ng + j * ng + k] *= jac;
+// 
+//           x[i * ng * ng + j * ng + k] = 0.;
+//           y[i * ng * ng + j * ng + k] = 0.;
+//           z[i * ng * ng + j * ng + k] = 0.;
+// 
+//           for(unsigned ii = 0; ii < 8; ii++) {
+//             x[i * ng * ng + j * ng + k] += phi[ii] * xv[0][ii];
+//             y[i * ng * ng + j * ng + k] += phi[ii] * xv[1][ii];
+//             z[i * ng * ng + j * ng + k] += phi[ii] * xv[2][ii];
+//           }
+// 
+//         }
+//       }
+//     }
+// 
+//     std::cout.precision(14);
+//     std::cout << "const double tet_gauss::Gauss" << ng - 1 << "[" << dim + 1 << "][" <<  size << "] = {{";
 //     for(unsigned i = 0; i < ng; i++) {
 //       for(unsigned j = 0; j < ng; j++) {
 //         for(unsigned k = 0; k < ng; k++) {
@@ -159,150 +342,72 @@ int main(int argc, char** args) {
 // 
 //   }
 // 
-// 
-// //triangle
-//   
-//     std::vector < std::vector < double > > xv = {{0., 1., 0., 0.}, {0., 0., 1., 1.}};
-//     std::vector <double> phi;  // local test function
-//     std::vector <double> phi_x; // local test function first order partial derivatives
-//     double jac;
-// 
-//     const elem_type *fem = new const elem_type_2D("quad", "linear", "seventh");
-// 
-//     dim = 2;
-//     maxNG = 3;
-// 
-//     std::cout << "const unsigned tri_gauss::GaussPoints[" << maxNG << "] = {";
-//     for(unsigned ng = 1; ng <= maxNG; ng++) {
-//       unsigned size = pow(ng, dim);
-//       std::cout << size << ", ";
-//     }
-//     std::cout << "\b\b};\n\n";
-// 
-//     std::cout << "const double * tri_gauss::Gauss[" << maxNG << "] = {";
-//     for(unsigned ng = 1; ng <= maxNG; ng++) {
-//       std::cout << "Gauss" << ng - 1 << "[0], ";
-//     }
-//     std::cout << "\b\b};\n\n";
-// 
-//     for(unsigned ng = 1; ng <= maxNG; ng++) {
-// 
-//       unsigned size = pow(ng, dim);
-//       std::vector < double> weight(size);
-//       std::vector < double> x(size);
-//       std::vector < double> y(size);
-// 
-//       std::vector < double > xi(dim);
-//       for(unsigned i = 0; i < ng; i++) {
-//         for(unsigned j = 0; j < ng; j++) {
-//           weight[i * ng + j] = Gauss[ng - 1][i] * Gauss[ng - 1][j];
-//           xi[0] = Gauss[ng - 1][ng + i];
-//           xi[1] = Gauss[ng - 1][ng + j];
-// 
-//           fem->Jacobian(xv, xi, jac, phi, phi_x);
-//           weight[i * ng + j] *= jac;
-// 
-//           x[i * ng + j] = 0.;
-//           y[i * ng + j] = 0.;
-//           for(unsigned ii = 0; ii < 4; ii++) {
-//             x[i * ng + j] += phi[ii] * xv[0][ii];
-//             y[i * ng + j] += phi[ii] * xv[1][ii];
-//           }
-// 
-// 
-//         }
-//       }
-//       std::cout.precision(14);
-//       std::cout << "const double tri_gauss::Gauss" << ng - 1 << "[" << dim + 1 << "][" <<  size << "] = {{";
-//       for(unsigned i = 0; i < ng; i++) {
-//         for(unsigned j = 0; j < ng; j++) {
-//           std::cout << weight[i * ng + j] << ", ";
-//         }
-//       }
-//       std::cout << "\b\b},\n{";
-// 
-//       for(unsigned i = 0; i < ng; i++) {
-//         for(unsigned j = 0; j < ng; j++) {
-//           std::cout << x[i * ng + j] << ", ";
-//         }
-//       }
-//       std::cout << "\b\b},\n{";
-// 
-//       for(unsigned i = 0; i < ng; i++) {
-//         for(unsigned j = 0; j < ng; j++) {
-//           std::cout << y[i * ng + j] << ", ";
-//         }
-//       }
-//       std::cout << "\b\b}};" << std::endl << std::endl;
-// 
-//     }
-// 
-//     delete fem;
-// 
-//   
+//   delete fem;
 
-//tetrahedra
+
   
-    std::vector < std::vector < double > > xv = {{0,1,0,0,0,0,0,0},{0,0,1,1,0,0,0,0},{0,0,0,0,1,1,1,1}};
-    std::vector <double> phi;  // local test function
-    std::vector <double> phi_x; // local test function first order partial derivatives
-    double jac;
+  //tetrahedra
 
-    const elem_type *fem = new const elem_type_3D("hex", "quadratic", "seventh");
+  std::vector < std::vector < double > > xv = {{0, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 1, 1, 0, 0, 1, 1}, {-1, -1, -1, -1, 1, 1, 1, 1}};
+  std::vector <double> phi;  // local test function
+  std::vector <double> phi_x; // local test function first order partial derivatives
+  double jac;
 
-    dim = 3;
-    maxNG = 3;
+  const elem_type *fem = new const elem_type_3D("hex", "linear", "seventh");
 
-    std::cout << "const unsigned tet_gauss::GaussPoints[" << maxNG << "] = {";
-    for(unsigned ng = 1; ng <= maxNG; ng++) {
-      unsigned size = pow(ng, dim);
-      std::cout << size << ", ";
-    }
-    std::cout << "\b\b};\n\n";
+  dim = 3;
+  maxNG = 3;
 
-    std::cout << "const double * tet_gauss::Gauss[" << maxNG << "] = {";
-    for(unsigned ng = 1; ng <= maxNG; ng++) {
-      std::cout << "Gauss" << ng - 1 << "[0], ";
-    }
-    std::cout << "\b\b};\n\n";
+  std::cout << "const unsigned tet_gauss::GaussPoints[" << maxNG << "] = {";
+  for(unsigned ng = 1; ng <= maxNG; ng++) {
+    unsigned size = pow(ng, dim);
+    std::cout << size << ", ";
+  }
+  std::cout << "\b\b};\n\n";
 
-    for(unsigned ng = 1; ng <= maxNG; ng++) {
-    
-      unsigned size = pow(ng, dim);
-      std::vector < double> weight(size);
-      std::vector < double> x(size);
-      std::vector < double> y(size);
-      std::vector < double> z(size);
-      std::vector < double > xi(dim);
-      
-      for(unsigned i = 0; i < ng; i++) {
-        for(unsigned j = 0; j < ng; j++) {
-          for(unsigned k = 0; k < ng; k++) {
-            weight[i * ng * ng + j * ng + k] = Gauss[ng - 1][i] * Gauss[ng - 1][j] * Gauss[ng - 1][k];
-            xi[0] = Gauss[ng - 1][ng + i];
-            xi[1] = Gauss[ng - 1][ng + j];
-            xi[2] = Gauss[ng - 1][ng + k];
-            
+  std::cout << "const double * tet_gauss::Gauss[" << maxNG << "] = {";
+  for(unsigned ng = 1; ng <= maxNG; ng++) {
+    std::cout << "Gauss" << ng - 1 << "[0], ";
+  }
+  std::cout << "\b\b};\n\n";
 
-            fem->Jacobian(xv, xi, jac, phi, phi_x);
-            
-            weight[i * ng * ng + j * ng + k] *= jac;
+  for(unsigned ng = 1; ng <= maxNG; ng++) {
 
-            x[i * ng * ng + j * ng + k] = 0.;
-            y[i * ng * ng + j * ng + k] = 0.;
-            z[i * ng * ng + j * ng + k] = 0.;
-            
-            for(unsigned ii = 0; ii < 24; ii++) {
-              x[i * ng * ng + j * ng + k] += phi[ii] * xv[0][ii];
-              y[i * ng * ng + j * ng + k] += phi[ii] * xv[1][ii];
-              z[i * ng * ng + j * ng + k] += phi[ii] * xv[2][ii];
-            }
+    unsigned size = pow(ng, dim);
+    std::vector < double> weight(size);
+    std::vector < double> x(size);
+    std::vector < double> y(size);
+    std::vector < double> z(size);
+    std::vector < double > xi(dim);
 
+    for(unsigned i = 0; i < ng; i++) {
+      for(unsigned j = 0; j < ng; j++) {
+        for(unsigned k = 0; k < ng; k++) {
+          weight[i * ng * ng + j * ng + k] = Gauss[ng - 1][i] * Gauss[ng - 1][j] * Gauss[ng - 1][k];
+          xi[0] = Gauss[ng - 1][ng + i];
+          xi[1] = Gauss[ng - 1][ng + j];
+          xi[2] = Gauss[ng - 1][ng + k];
+
+
+          fem->Jacobian(xv, xi, jac, phi, phi_x);
+
+          weight[i * ng * ng + j * ng + k] *= jac;
+
+          x[i * ng * ng + j * ng + k] = 0.;
+          y[i * ng * ng + j * ng + k] = 0.;
+          z[i * ng * ng + j * ng + k] = 0.;
+
+          for(unsigned ii = 0; ii < 8; ii++) {
+            x[i * ng * ng + j * ng + k] += phi[ii] * xv[0][ii];
+            y[i * ng * ng + j * ng + k] += phi[ii] * xv[1][ii];
+            z[i * ng * ng + j * ng + k] += phi[ii] * xv[2][ii];
           }
+
         }
       }
-   
- std::cout.precision(14);
+    }
+
+    std::cout.precision(14);
     std::cout << "const double tet_gauss::Gauss" << ng - 1 << "[" << dim + 1 << "][" <<  size << "] = {{";
     for(unsigned i = 0; i < ng; i++) {
       for(unsigned j = 0; j < ng; j++) {
@@ -340,9 +445,10 @@ int main(int argc, char** args) {
     }
     std::cout << "\b\b}};" << std::endl << std::endl;
 
-    }
+  }
 
-delete fem;
+  delete fem;
+
 
 
 
