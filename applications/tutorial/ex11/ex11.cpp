5cc801a5fc4f7d64cb1d731a431547e8aa0adadf
erkara <erdi_kara88@hotmail.com>
2020-10-21 16:26:39 -0500

    minor changes

--- applications/tutorial/ex11/ex11.cpp
+++ applications/tutorial/ex11/ex11.cpp
@@ -2,61 +2,113 @@
 #include <math.h>
 #include <stdio.h>
 #include "mpi.h"
-#include <vector>
 
 // MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+// buf: starting adress of the buffer
+// count: number of elements in that buffer
+// dest: ID of the receiver
+// tag: tag the messages
 // MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,MPI_Comm comm, MPI_Status * status)
 
 
+void SimpleMPI1();
+void SimpleSR();
+void SRMultiply();
 int main(int argc, char** argv) {
 
-//   int my_PE_num, numbertoreceive, numbertosend = 42;
-//   MPI_Status status;
-//
-//   MPI_Init(&argc, &argv);
-//
-//   MPI_Comm_rank(MPI_COMM_WORLD, &my_PE_num);
-//
-//   if(my_PE_num == 0) {
-//
-//     MPI_Recv(&numbertoreceive, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-//
-//     std::cout << "NumberReceived = " << numbertoreceive << std::endl;
-//   }
-//   else {
-//     std::cout << "I am PE = " << my_PE_num << std::endl;
-//     MPI_Send(&numbertosend, 1, MPI_INT, 0, 10, MPI_COMM_WORLD);
-//   }
-//   MPI_Finalize();
 
-  std::vector < std::vector < std::vector < double > > > T;
-  unsigned N = 5;
-  unsigned dim = 3;
+  MPI_Init(&argc, &argv);
 
+  SRMultiply();
 
-  T.reserve(pow(4 * N,dim));
-  for(unsigned i = 0; i < N; i++) {
-    T[i].resize(dim - 1);
-    for(unsigned k = 0; k < dim - 1; k++) {
-      T[i][k].resize(dim, 0.);
-    }
-  }
-
-  for(unsigned cnt = 0; cnt < N ; cnt++) {
-    T[cnt][0][0] = 1.;
-    T[cnt][0][1] = 2.;
-    T[cnt][0][2] = 3.;
-
-    T[cnt][1][0] = 4.;
-    T[cnt][1][1] = 5.;
-    T[cnt][1][2] = 6.;
-    
-  }
-
-  std::cout << T[N-2][1][1] << " " << T.size() <<std::endl;
+  MPI_Finalize();
 
 
 }
 
 
+
+void SimpleMPI1() {
+  int iproc;
+  int numprocs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  std::cout << " This process # " << iproc << " / " << numprocs << std::endl;
+
+}
+
+
+// PE0 sends pi to others and they print once they get it.
+void SimpleSR() {
+  int iproc;
+  int numprocs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+  if(iproc == 0) {
+    double pi = 3.14;
+    for(int index = 1; index < numprocs ; index++) {
+      MPI_Send(&pi, 1, MPI_DOUBLE, index, 0,  MPI_COMM_WORLD);
+      std::cout << " Sent to : " << index << std::endl;
+    }
+  }
+  else {
+    // carefully understand processes are waiting iproc=0 to send the info
+    double value;
+    MPI_Status status;
+    int ierr = MPI_Recv(&value, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    std::cout << " Received by : " << iproc << std::endl;
+  }
+}
+
+// PE0 sends 10 and they multiply it by their ID and send back to 0 where
+// its sum is calcuated and printed. Faster with broadcast
+
+void SRMultiply() {
+  
+  int iproc;
+  int numprocs;
+  int result = 0;
+  
+  MPI_Status status;
+  MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+  if(iproc == 0) {
+    int number = 10;
+    for(int index = 1; index < numprocs ; index++) {
+      MPI_Send(&number, 1, MPI_DOUBLE, index, 0,  MPI_COMM_WORLD);
+      std::cout << " Sent to : " << index << std::endl;
+    }
+  }
+  else {
+    int value;
+    MPI_Recv(&value, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    result = value * iproc;
+    std::cout << "Result on ID : " << iproc << " =  " << result << std::endl;
+  }
+  
+  
+
+  if(iproc == 0) {
+    for(int index = 1; index < numprocs ; index++) {
+      int value;
+      MPI_Recv(&value, 1, MPI_DOUBLE, index, 0, MPI_COMM_WORLD, &status);
+      result += value;
+    }
+    std::cout << " Total =  " << result << std::endl;
+  }
+  else {
+    MPI_Send(&result, 1, MPI_DOUBLE, 0, 0,  MPI_COMM_WORLD);
+  }
+
+
+}
+
+
+
+void DotProd(){
+    std::vector<int> u = {2,4,5}; 
+    std::vector<int> v = {4,3,6};
+}
 
