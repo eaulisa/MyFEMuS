001d3c2f6facddfdb1c248eccdf2a5b8722e032c
eaulisa <eugenio.aulisa@ttu.edu>
2020-12-08 06:29:12 -0600

    testing the parallel

--- applications/NonLocal/ex10/ex10.cpp
+++ applications/NonLocal/ex10/ex10.cpp
@@ -1,62 +1,20 @@
 
-#include "FemusInit.hpp"
-#include "MultiLevelProblem.hpp"
-#include "VTKWriter.hpp"
-#include "TransientSystem.hpp"
-#include "NonLinearImplicitSystem.hpp"
+#include "mpi.h"
+#include <iostream>
 
-#include "NumericVector.hpp"
-#include "adept.h"
+int main(int argc, char* argv[]) {
 
-#include "petsc.h"
-#include "petscmat.h"
-#include "PetscMatrix.hpp"
+  // init Petsc-MPI communicator
+  MPI_Init(&argc, &argv);
+  int p;
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+  int myid;
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);   
+  if(myid == 0){
+    std::cout <<"The total number of Processes is " << p <<std::endl;
+  }
+  MPI_Finalize();
 
-#include "slepceps.h"
-
-#include "../include/nonlocal_assembly_adaptive.hpp"
-
-//2D NONLOCAL EX : nonlocal diffusion for a body with different material properties
-
-using namespace femus;
-
-double InitalValueU(const std::vector < double >& x) {
-  double value;
-
-  //value =  x[0] * x[0] + x[1] * x[1]; //consistency 
-  value =  x[0] * x[0] * x[0] + x[1] * x[1] * x[1]; //cubic 
-  //value = x[0] * x[0] * x[0] * x[0] + x[1] * x[1] * x[1] * x[1]; //quartic
-
-
-
-
-  return value;
-}
-
-void GetL2Norm(MultiLevelSolution & mlSol, MultiLevelSolution & mlSolFine);
-
-bool SetBoundaryCondition(const std::vector < double >& x, const char SolName[], double& value, const int facename, const double time) {
-
-  bool dirichlet = true;
-  //value = x[0] * x[0] + x[1] * x[1]; //consistency
-  value = x[0] * x[0] * x[0] + x[1] * x[1] * x[1]; //cubic
-  //value = x[0] * x[0] * x[0] * x[0] + x[1] * x[1] * x[1] * x[1]; //quartic
-
-
-  return dirichlet;
-}
-
-//unsigned numberOfUniformLevels = 2; //consistency
-unsigned numberOfUniformLevels = 1; //cubic-quartic 2->6 //cubic Marta4Quad Tri Mix 
-//unsigned numberOfUniformLevels = 2; //cubic-quartic 2->4 mappa a 4->6 //cubic Marta4Fine 
-
-
-unsigned numberOfUniformLevelsFine = 1;
-
-int main(int argc, char** argv) {
-
-  FemusInit mpinit(argc, argv, MPI_COMM_WORLD);
-  std::cout << "Hello\n"<<std::flush;
   return 0;
 
 } //end main
