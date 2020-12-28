b66e53dacde119f2ee4432fd38305f9b7f5c9ea5
eaulisa <eugenio.aulisa@ttu.edu>
2020-12-27 18:01:04 -0600

    starting using polyLog from lisk

--- applications/tutorial/eqPoly/eqPoly.cpp
+++ applications/tutorial/eqPoly/eqPoly.cpp
@@ -24,9 +24,105 @@
 #include <iomanip>
 #include "./LiSK/lisk.hpp"
 
-int main(int argc, const char * argv[]) {
+
+
+#include <iostream>
+#include <stdio.h>      /* printf */
+#include <math.h>       /* exp */
+using namespace std;
+
+
+
+
+// //Fake function just to be a place holder for the Lisk Li function
+// int Li(int i, double j)
+// {
+//   return i * j;
+// }
+
+// This function accepts: the diminsion, dim; polynomial degree, degree; generalized Heavyside funcion paprameter, p;
+// discontinuity (point, line, or plane) ax + by + cz = d, where b and c are set to zero by default;
+// The function returns an array of the equivalent polynomial coefficients, coefficients
+complex<double> * getCoefficients (const int &dim, const int &degree, const double &p, const double & a, const double & d, const double b = 0, const double c = 0)
+{
+  LiSK::LiSK< complex<double> > lisk(4);
+
+  if(dim == 1) {
+
+    //TODO
+
+    if(degree == 3) {
+
+      double x1r = -exp(p - d * p) ;
+      const complex<double> x1(x1r, 0.);
+
+      double x2r = -exp((-1 - d) * p);
+      const complex<double> x2(x2r, 0.);
+
+
+      static complex < double > coefficients[4];
+
+      coefficients[0] = 2. * (-1. + (2. * p + log(1. + exp(-1. + d) * p)) - log(1. + exp((1. + d) * p))) / p;
+
+      coefficients[1] = (2. * (p * log(1. + exp((-1. - d) * p)) + p * log(1. + exp(p - d * p)) - lisk.Li(2, x2) + lisk.Li(2, x1))) / pow(p, 2);
+
+      coefficients[2] = -2. / 3. + (2. * (-(pow(p, 2) * log(1. + exp((-1. - d) * p))) + pow(p, 2) * log(1. + exp(p - d * p)) +
+                                          2. * p * lisk.Li(2, x2) + 2. * p * lisk.Li(2, x1) + 2. * lisk.Li(3, x2) -
+                                          2. * lisk.Li(3, x1))) / pow(p, 3);
+
+      coefficients[3] = (2. * (pow(p, 3) * log(1. + exp((-1. - d) * p)) + pow(p, 3) * log(1. + exp(p - d * p)) - 3. * pow(p, 2) * lisk.Li(2, x2) +
+                               3. * pow(p, 2) * lisk.Li(2, x1) - 6. * p * lisk.Li(3, x2) - 6. * p * lisk.Li(3, x1) -
+                               6. * lisk.Li(4, x2) + 6. * lisk.Li(4, x1))) / pow(p, 4);
+
+      return coefficients;
+    }
+
+  }
+
+  else if(dim == 2) {
+    double coefficients[((degree + 1) * (degree + 2)) / 2];
+
+    //TODO
+
+  }
+
+  else if (dim == 3) {
+    double coefficients[((degree + 1) * (degree + 2) * (degree + 3)) / 6];
+
+    //TODO
+
+
+  }
+
+
+}
+
+int main ()
+{
+  complex < double > * ptr = getCoefficients(1, 3, 100, 1, 0.5);
+  std::cout<< ptr[0] << " "<< ptr[1] << " "<<  ptr[2]<< " "<<  ptr[3] << std::endl;
+  std::cout<< ptr[0].real() << " "<< ptr[1].real() << " "<<  ptr[2].real()<< " "<<  ptr[3].real() << std::endl;
+  return 0;
+}
+
+/*
+using namespace cln;*/
+
+int main1(int argc, const char * argv[]) {
 
   using namespace std;
+
+  complex<double> x(1 / 4., 1 / 4.);
+  double xr = 1.;
+  double xi = 1.;
+  x = complex <double> (xr, xi);
+
+  x = complex <double> (real(x), imag(x));
+
+  x.real(2.);
+
+  std::cout << x << std::endl;
+
 
   // Double precision example for Li_n and Li_22
   try {
@@ -34,7 +130,10 @@ int main(int argc, const char * argv[]) {
     //computations of Li_n up to n=20
     LiSK::LiSK<complex<double>> lisk(20);
 
-    const complex<double> x(1 / 4., 1 / 4.);
+    complex<double> x(1 / 4., 1 / 4.);
+    double xr = 1.;
+    double xi = 1.;
+    x = complex <double> (xr, xi);
     const vector<int> weights = {1, 2, 3, 4, 5, 6, 10, 15, 20};
 
     // Compute Li_n(x) for n=1,2,3,4,5,6,10,15,20 at x=1/4+I*1/4
