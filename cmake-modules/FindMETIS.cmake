e58536bca4f089bccdb37cf92f9fa9b0bd18873d
erkara <erkara@localhost.localdomain>
2021-06-10 12:42:16 -0500

    made changes in cmake files

--- cmake-modules/FindMETIS.cmake
+++ cmake-modules/FindMETIS.cmake
@@ -54,8 +54,11 @@ else(METIS_LIBRARIES AND METIS_INCLUDE_DIRS)
     set(metis_inc_names "metis.h")
     
     # we look into petsc externalpackage (for 3.5.2 version, to be improved)
-    find_path(METIS_INCLUDE_DIR ${metis_inc_names} "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/metis-5.1.0-p1/build/include/")
-        
+    #find_path(METIS_INCLUDE_DIR ${metis_inc_names} "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/metis-5.1.0-p1/build/include/")
+    find_path(METIS_INCLUDE_DIR ${metis_inc_names} "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/git.metis/petsc-build/include/")
+    
+    
+    
     if (METIS_INCLUDE_DIR)
 
         if (EXISTS "${METIS_INCLUDE_DIR}")
@@ -118,7 +121,9 @@ else(METIS_LIBRARIES AND METIS_INCLUDE_DIRS)
     set(metis_lib_names "metis")
 
     # we look into petsc externalpackage (for 3.5.2 version, to be improved)
-    set(METIS_LIBRARY_DIR "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/metis-5.1.0-p1/build/libmetis/")
+    #set(METIS_LIBRARY_DIR "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/metis-5.1.0-p1/build/libmetis/")
+    set(METIS_LIBRARY_DIR "${PETSC_DIR}/${PETSC_ARCH}/externalpackages/git.metis/petsc-build/libmetis/")
+    
     
     if (METIS_LIBRARY_DIR)
 
