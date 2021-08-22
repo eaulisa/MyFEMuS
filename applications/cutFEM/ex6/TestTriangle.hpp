96133364426f27db4cb7198e7fc22f5cea0281d8
eaulisa <eugenio.aulisa@ttu.edu>
2021-08-22 06:25:51 -0500

    triangle finished

--- applications/cutFEM/ex6/TestTriangle.hpp
+++ applications/cutFEM/ex6/TestTriangle.hpp
@@ -7,7 +7,7 @@ void TestTriangle(const Float1 &eps) {
   myType eps1 = 1.0e-12;
 
   std::cout << "testing the Triangle \n";
-  std::vector<unsigned>m = {13, 5};
+  std::vector<unsigned>m = {9, 9};
   //std::vector<myType>a = { -1e-05, -1e-5};
   std::vector<myType>a = { 0., 0. };
   std::vector<myTypeB> af(2);
@@ -17,26 +17,57 @@ void TestTriangle(const Float1 &eps) {
   myType d = 1;
   myTypeB df = d;
 
-  int s = 6;
+  int s = 4;
 
-  std::cout.precision(14);
+  std::cout.precision(16);
 
-  std::cout << "a+b+d = "<<a[0] + a[1] +d <<std::endl;
-  std::cout << "d = " << d << std::endl;
-  std::cout << "d/b = "<<d/a[1] << std::endl;
-  std::cout << "|a+b|/|a-b| = "  <<fabs((a[0]+a[1])/(a[0]-a[1]))<<std::endl;
-  
-  std::cout << "s + m + n= " << s+m[0]+m[1] << " a = " << a[0] << " b = " << a[1] << " d = " << d << std::endl;
-  std::cout << TriangleA(s, m, af, df) << std::endl;
-  std::cout << TriangleA(s, m, a, d) << std::endl;
-  std::cout << fabs((TriangleA(s, m, af, df)- TriangleA(s, m, a, d))/TriangleA(s, m, af, df)) << std::endl; 
-  
-  std::cout << TriangleFull(s, m, af, df) << std::endl;
-  std::cout << TriangleFull(s, m, a, d) << std::endl;
-  std::cout << fabs((TriangleFull(s, m, af, df)- TriangleFull(s, m, a, d))/TriangleFull(s, m, af, df)) << std::endl; 
-  
-  
-  //return;
+//   std::cout << "a+b+d = " << a[0] + a[1] + d << std::endl;
+//   std::cout << "d = " << d << std::endl;
+//   std::cout << "d/b = " << d / a[1] << std::endl;
+//   std::cout << "|a+b|/|a-b| = "  << fabs((a[0] + a[1]) / (a[0] - a[1])) << std::endl;
+// 
+//   std::cout << "s + m + n= " << s + m[0] + m[1] << " a = " << a[0] << " b = " << a[1] << " d = " << d << std::endl;
+//   std::cout << TriangleA(s, m, af, df) << std::endl;
+//   std::cout << TriangleA(s, m, a, d) << std::endl;
+//   std::cout << fabs((TriangleA(s, m, af, df) - TriangleA(s, m, a, d)) / TriangleA(s, m, af, df)) << std::endl;
+// 
+//   std::cout << TriangleFull(s, m, af, df) << std::endl;
+//   std::cout << TriangleFull(s, m, a, d) << std::endl;
+//   std::cout << fabs((TriangleFull(s, m, af, df) - TriangleFull(s, m, a, d)) / TriangleFull(s, m, af, df)) << std::endl;
+
+  std::vector<std::vector<myType>> epsCut{{0, 0, 0},
+    {eps1, 0, 0}, {-eps1, 0, 0}, {0, eps1, 0}, {0, -eps1, 0}, {0, 0, eps1}, {0, 0, -eps1},
+    {eps1, eps1, 0}, {eps1, -eps1, 0}, {-eps1, eps1, 0}, {-eps1, -eps1, 0},
+    {eps1, eps1, 0}, {eps1, -eps1, eps1}, {-eps1, eps1, 0}, {-eps1, -eps1, eps1},
+    {eps1, eps1, 0}, {eps1, -eps1, -eps1}, {-eps1, eps1, 0}, {-eps1, -eps1, -eps1}
+  };
+  std::vector<std::vector<myType>> smallCut{{0, 0, 0}, {0, 0, 1}, {0, 0, -1}, {-1, -1, 0}, {-1, 1, 0}, {1, 1, -2}, {1, 0, -1}, {1, -1, -2}, {0, -1, 0}};
+  for(unsigned i = 0; i < smallCut.size(); i++) {
+    for(unsigned j = 0; j < epsCut.size(); j++) {
+
+      a[0] = smallCut[i][0] + epsCut[j][0];
+      a[1] = smallCut[i][1] + epsCut[j][1];
+      d = smallCut[i][2] + epsCut[j][2];
+
+      af[0] = static_cast<myType>(a[0]);
+      af[1] = static_cast<myType>(a[1]);
+      df = static_cast<myType>(d);
+
+      myType I1 = TriangleA(s,  m, a, d);
+      myTypeB I2 = TriangleA(s,  m, af, df);
+      if((I2 != 0. &&  fabs((I1 - I2) / I2) < eps) || I1 == 0.) {
+        std::cout << "passed ";
+        std::cout << "s = " << s << " a = " << a[0] << " b = " << a[1] << " d = " << d << " I = " << I1 << std::endl;
+      }
+      else {
+        std::cout << "Warning failed ";
+        std::cout << "s = " << s << " a = " << a[0] << " b = " << a[1] << " d = " << d << " I1 =" << I1 << " I2 = " << I2 << std::endl;
+      }
+    }
+    std::cout<<std::endl;
+  }
+
+  /*
   for(unsigned j = 0; j < 2; j++) {
 
     myType c1 = (j == 0) ? eps1 : 1.;
@@ -429,7 +460,7 @@ void TestTriangle(const Float1 &eps) {
     }
   }
 
-
+*/
 
 
 
