a55bb5c93180192124cda19c8e2067d9ba712b97
Himali-Gammanpila <himaligammanpila@gmail.com>
2022-05-04 13:16:32 -0500

    ghost penalty parellel comment

--- applications/Nitsche/ex11/GhostPenalty.hpp
+++ applications/Nitsche/ex11/GhostPenalty.hpp
@@ -25,34 +25,34 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
   adept::Stack& s = FemusInit::_adeptStack;
 
   const unsigned dim = msh->GetDimension();
-  const unsigned dim2 = 3 * (dim - 1);
-
+  const unsigned dim2 = 3 * (dim - 1);//Dimension of the second order derivative
+  // this is independent. In 2D-> xx,yy,xy , In 3D-> xx,yy,zz,xy,xz,yz
   // data
   unsigned iproc  = msh->processor_id();
-  unsigned nprocs  = msh->n_processors();//**??
+  unsigned nprocs  = msh->n_processors();//Number of processors that are running 
 
   //quantities for iel will have index1
   //quantities for jel will have index2
   //Only the bool omega1 refers to subdomain 1 and 2 in the Nietsche coupling
 
-  //**** it is solution of one element and neighboring element
-  vector< adept::adouble > solu1; // local solution (velocity)//***is it just a vector?
-  vector< adept::adouble > solu2; // local solution (velocity)
+  //solution of one element and neighboring element
+  vector< adept::adouble > solu1; 
+  vector< adept::adouble > solu2; 
 
-  vector< adept::adouble > aRes1;     // local redidual vector//*****is it a vector of 1?
+  vector< adept::adouble > aRes1;     // local redidual vector
   vector< adept::adouble > aRes2;     // local redidual vector
 
   vector< double > rhs; // local redidual vector
   vector < double > Jac;
 
-  std::vector <unsigned> sysDofs;//*****is it a vector of 1? No
+  std::vector <unsigned> sysDofs;
 
 
   double weight;
   std::vector < double > phi;
   std::vector < double> gradPhi;
 
-  //*** referes to solution of the element? is it also for neighboring elements?
+  
   double weight1;
   std::vector < double > phi1;
   std::vector < double> gradPhi1;
@@ -76,12 +76,12 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
   //variable-name handling
   const char varname[2][3] = {"u1", "u2"};//vector of strings. two entries and atmost 3 characters
 
-  //***if omega1 is true, it's going to give you u1. if it is false, it is u2.
+  //if omega1 is true, it's going to give you u1. if it is false, it is u2.
   unsigned indexSol = mlSol->GetIndex(&varname[!omega1][0]);
   unsigned indexPde = mlPdeSys.GetSolPdeIndex(&varname[!omega1][0]);
   unsigned solType = mlSol->GetSolutionType(&varname[0][0]);
 
-  unsigned eflagIndex = mlSol->GetIndex("eflag");//**will tell you whether we have a cut or not. if eflag=1 we have a cut cell.
+  unsigned eflagIndex = mlSol->GetIndex("eflag");//whether you have a cut or not. if eflag=1 we have a cut cell.
 
   start_time = clock();
 
@@ -90,12 +90,12 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
 
 
   //flagmark
-  //**here we are looping on the element on the mesh
+  //loop on the element on the mesh
   for(int iel = msh->_elementOffset[iproc]; iel < msh->_elementOffset[iproc + 1]; iel++) {
-    //***if we have a cut element go in
+    //if we have a cut element go in
     unsigned eFlag1 = static_cast <unsigned>(floor((*mysolution->_Sol[eflagIndex])(iel) + 0.25));
     if(eFlag1 == 1) {
-      //**if it is a cut element we need to know what type of cut element we have and number of nodes we have.
+      //in cut element we need to know what type of cut element we have and number of nodes we have.
       short unsigned ielt1 = msh->GetElementType(iel);
       unsigned nDofs1 = msh->GetElementDofNumber(iel, solType);    // number of solution element dofs
       // resize local arrays
@@ -110,7 +110,7 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
         solu1[i] = (*mysolution->_Sol[indexSol])(idof);
         sysDofs[i] = myLinEqSolver->GetSystemDof(indexSol, indexPde, i, iel); //local to global mapping for the PDE
       }
-
+      //coordinates of iel element
       for(unsigned i = 0; i < nDofs1; i++) {
         unsigned idofX = msh->GetSolutionDof(i, iel, 2);
         for(unsigned  k = 0; k < dim; k++) {
@@ -119,16 +119,19 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
       }
 
       bool aP1IsInitialized = false;
-      //**looping on faces of element iel
+      //looping on faces of element iel
       for(unsigned iface = 0; iface < msh->GetElementFaceNumber(iel); iface++) {
-        int jel = el->GetFaceElementIndex(iel, iface) - 1;//**jel is the neighboring element
+        int jel = el->GetFaceElementIndex(iel, iface) - 1;//jel is the neighboring element
         if(jel >= 0) { // iface is not a boundary of the domain.
-          //**if we are on the boundary jel is negative number.we ignore that case
+          //if we are on the boundary jel is negative number.we ignore that case
+            
           unsigned jproc = msh->IsdomBisectionSearch(jel, 3);
-          if(jproc == iproc) {//**iel and jel belongs to the same processor
-            //**checking flag of jel
+          //jel is piece wise discontinuous FEM it is 3. 0=Lagrange Linear, 1=Lagrange quadratic, 2=Lagrage biquadratic, 3= constat piecewise discontinuous linear. 
+          if(jproc == iproc) {//iel and jel belongs to the same processor
+            //since we are ine the same processor, we can exract flag jel
             unsigned eFlag2 = static_cast <unsigned>(floor((*mysolution->_Sol[eflagIndex])(jel) + 0.25));
-            //**if you are a interior cell of same type or if it is a cut element with jel>iel (as we want to do integral one type)
+            
+             //if you are in omega1, do ghost cell in 0,1 , if you are in omega2 do 1,2 with jel>iel (as we want to do integral one type)
             if(eFlag2 == !omega1 * 2 || (eFlag2 == 1 && jel > iel)) {
 
               short unsigned ielt2 = msh->GetElementType(jel);
@@ -151,12 +154,13 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
                   vx2[k][i] = (*msh->_topology->_Sol[k])(idofX);
                 }
               }
-              //**now we are on iel and on a particular face and corresponding jel. we have all the information.
+              //now we are on iel and on a particular face and corresponding jel. we have all the information.
               aRes1.assign(nDofs1, 0.);
               aRes2.assign(nDofs2, 0.);
 
               s.new_recording();
-              //**gathering information on the boundary
+               
+              //gathering information on the boundary
               const unsigned faceGeom = msh->GetElementFaceType(iel, iface);
               unsigned faceDofs = msh->GetElementFaceDofNumber(iel, iface, solType);
               std::vector  < std::vector  <  double > > faceVx(dim);    // A matrix holding the face coordinates rowwise.
@@ -338,23 +342,27 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
   if(nprocs > 1) {
     for(unsigned kproc = 0; kproc < nprocs; kproc++) { // not scalable
       for(int iel = msh->_elementOffset[kproc]; iel < msh->_elementOffset[kproc + 1]; iel++) {
-
+        //**who owns eFlag1? iproc. one of kproc is iproc. 
         unsigned eFlag1;
 
         if(iproc == kproc) {
           eFlag1 = static_cast <unsigned>(floor((*mysolution->_Sol[eflagIndex])(iel) + 0.25));
         }
+        //**broadcasting to all other processors eFlag1. they are expecting to recieve from kproc.
         MPI_Bcast(&eFlag1, 1, MPI_UNSIGNED, kproc, PETSC_COMM_WORLD);
-
+        
+        //**now every processor knows eFlag1
         if(eFlag1 == 1) {
+          //**only kproc knows the iformation about the faces.
           unsigned nFaces;
           if(iproc == kproc) {
             nFaces = msh->GetElementFaceNumber(iel);
           }
           MPI_Bcast(&nFaces, 1, MPI_UNSIGNED, kproc, PETSC_COMM_WORLD);
-
+          
+          //**looping on the faces 
           for(unsigned iface = 0; iface < nFaces; iface++) {
-
+            
             int jel;
             if(iproc == kproc) {
               jel = el->GetFaceElementIndex(iel, iface) - 1;
@@ -362,24 +370,30 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
             MPI_Bcast(&jel, 1, MPI_INT, kproc, PETSC_COMM_WORLD);
 
             if(jel >= 0) { // iface is not a boundary of the domain
+            //**then somebody should owns jel. then we check to which process jel belogs to 
               unsigned jproc = msh->IsdomBisectionSearch(jel, 3);  // return  jproc for piece-wise constant discontinuous type (3)
+              
+              //**if jproc is different from kproc and then if iproc == kproc || iproc == jproc 
               if(jproc != kproc && (iproc == kproc || iproc == jproc)) {
-
+              //**after this only jproc and kproc is working. So hereafter if we have exchanges we have to exchange only with in jproc and kproc.
                 unsigned eFlag2;
+                //**information about eFlag2 has jproc
                 if(iproc == jproc) {
                   eFlag2 = static_cast <unsigned>(floor((*mysolution->_Sol[eflagIndex])(jel) + 0.25));
-                  MPI_Send(&eFlag2, 1, MPI_UNSIGNED, kproc, 0, PETSC_COMM_WORLD);
+                  MPI_Send(&eFlag2, 1, MPI_UNSIGNED, kproc, 0, PETSC_COMM_WORLD);//**MPI_Send done by jproc to kproc
                 }
                 else if(iproc == kproc) {
                   MPI_Recv(&eFlag2, 1, MPI_UNSIGNED, jproc, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
                 }
 
                 if(eFlag2 == !omega1 * 2 || (eFlag2 == 1 && jel > iel)) {
+                //**both jproc and kproc going forward
+                  
                   //std::cout << "I am " << iel << " on " << kproc << " talking with " << jel << " on " << jproc << std::endl;
 
                   short unsigned ielt1;
                   short unsigned ielt2;
-
+                  //now jproc send all the information to iproc. and iproc does the essembly.
                   if(iproc == kproc) {
                     ielt1 = msh->GetElementType(iel);
                     MPI_Recv(&ielt2, 1, MPI_UNSIGNED_SHORT, jproc, 0, PETSC_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -388,6 +402,7 @@ void AssembleGhostPenaltyP(MultiLevelProblem& ml_prob, const bool &omega1) {
                     ielt2 = msh->GetElementType(jel);
                     MPI_Send(&ielt2, 1, MPI_UNSIGNED_SHORT, kproc, 0, PETSC_COMM_WORLD);
                   }
+                  
 
                   unsigned nDofs1;
                   unsigned nDofs2 = el->GetNVE(ielt2, solType);
