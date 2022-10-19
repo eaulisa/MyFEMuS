8138f15d1b7c9e98556a15c1dcefa1c177f417bf
eugenio aulisa <eaulisa@localhost.localdomain>
2022-10-19 14:48:29 -0500

    added second function to get intervals

--- applications/cutFEM/ex20/ex20.cpp
+++ applications/cutFEM/ex20/ex20.cpp
@@ -1,6 +1,8 @@
 
 #include <algorithm>
 #include <vector>
+#include <cmath>
+#include <algorithm>    // std::sort
 
 int main() {
   return 1;
@@ -10,6 +12,80 @@ int main() {
 double b(const double &p) {
   return std::max(std::min(p, 1.), 0.);
 }
+
+
+
+void GetInterval4(const std::vector <double> &a1, const std::vector <double> &a2, std::vector< std::pair<double, double> > &I1, std::vector< std::pair<double, double> > &I2, std::vector<std::pair<double, double>> &I3) {
+
+  std::vector <double> x(6);
+
+  x[0] = 0 ;
+  unsigned cnt = 1;
+
+  for(unsigned k = 0; k < 2; k++) {
+    const std::vector<double> &a = (k == 0) ? a1 : a2;
+    double delta = a[1] * a[1] - 4 * a[0] * a[2] ;
+    if(delta > 0) {
+      for(unsigned i = 0; i < 2; i++) {
+        double y = (-a[1] - pow(-1, i) * sqrt(delta)) / (2 * a[0]) ;
+        if(y > 0 && y < 1) {
+          x[cnt] = y ;
+          cnt++ ;
+        }
+      }
+    }
+  }
+
+  x[cnt] = 1 ;
+  cnt++;
+
+  x.resize(cnt);
+  std::sort(x.begin(), x.end());
+  I1.resize(0);
+  I2.resize(0);
+  I3.resize(0);
+
+  for(unsigned i = 0 ; i < cnt - 1 ; i++) {
+    double xm = (x[i] + x[i + 1]) * 0.5 ;
+    double f1 = a1[0] * xm * xm + a1[1] * xm + a1[2] ;
+    double f2 = a2[0] * xm * xm + a2[1] * xm + a2[2] ;
+    if(f1 > 0) {
+      if(f2 > 0) {
+        I3.resize(I3.size() + 1, std::pair<double, double>(x[i], x[i + 1]));
+      }
+      else {
+        I1.resize(I1.size() + 1, std::pair<double, double>(x[i], x[i + 1]));
+      }
+    }
+    else if(f2 > 0) {
+      I2.resize(I2.size() + 1, std::pair<double, double>(x[i], x[i + 1]));
+    }
+  }
+
+  for(unsigned i = 0; i < I1.size() - 1; i++) {
+    if(I1[i].second == I1[i + 1].first) {
+      I1[i].second = I1[i + 1].second;
+      I1.erase(I1.begin() + i + 1);
+    }
+  }
+  
+  for(unsigned i = 0; i < I2.size() - 1; i++) {
+    if(I2[i].second == I2[i + 1].first) {
+      I2[i].second = I2[i + 1].second;
+      I2.erase(I2.begin() + i + 1);
+    }
+  }
+  
+  for(unsigned i = 0; i < I3.size() - 1; i++) {
+    if(I3[i].second == I3[i + 1].first) {
+      I3[i].second = I3[i + 1].second;
+      I3.erase(I3.begin() + i + 1);
+    }
+  }
+
+
+}
+
 
 
 void GetInterval4(double p1, double p2, double q1, double q2, const double &k, std::vector< std::pair<double, double> > &I1, std::vector< std::pair<double, double> > &I2, std::vector<std::pair<double, double>> &I3) {
